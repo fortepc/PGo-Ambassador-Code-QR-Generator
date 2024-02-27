222ed90bc5494ceec04429834ef90e39215d4a57
Forte <jeffers.david93@gmail.com>
2024-02-27 02:01:22 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AmbassadorCodeQRGenerator',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add other dependencies if needed
+    ],
+)
